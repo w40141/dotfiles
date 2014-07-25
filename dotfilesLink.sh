d898005b91ca5aba2b42ad6fffc8224c3223df62
w40141 <w.40141@gmail.com>
2014-07-25 18:54:52 +0900

    .vim remove

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim ~/.vim
+# ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.zsh ~/.zsh
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
