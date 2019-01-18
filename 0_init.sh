ca15e1e196730b6abdd816f8f3081a4331e61c05
daisuke oku <w.40141@gmail.com>
2019-01-18 12:36:07 +0900

    add submodule, Makefile, init.sh

--- 0_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-# ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.zsh ~/.zsh
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zshrc.antigen ~/.zshrc.antigen
