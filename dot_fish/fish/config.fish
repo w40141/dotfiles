7b734022d6e5c197a4b0d1899e12b7025a3a11cc
daisuke oku <w.40141@gmail.com>
2019-01-21 15:56:23 +0900

    remove fzf keybind#

--- dot_fish/fish/config.fish
+++ dot_fish/fish/config.fish
@@ -9,15 +9,15 @@ function cd
   standard_cd $argv; and ls -a
 end
 
-# fzf
-# 一覧の上を最新として、境目をボーダーに
-set -x FZF_DEFAULT_OPTS "--reverse --border"
-# ディレクトリ検索で選択した項目の配下の構成をプレビューに表示する
-set -x FZF_ALT_C_OPTS   "--preview 'tree -C {} | head -200'"
-# ファイル検索で選択した項目の中身をプレビューに表示する
-set -x FZF_CTRL_T_OPTS  "--preview 'head -100 {}'"
-
-# エラーコードは番号で表示する
-set -g theme_show_exit_status yes
-# Gitのahead情報を細かく表示する
-set -g theme_display_git_ahead_verbose yes
+# # fzf
+# # 一覧の上を最新として、境目をボーダーに
+# set -x FZF_DEFAULT_OPTS "--reverse --border"
+# # ディレクトリ検索で選択した項目の配下の構成をプレビューに表示する
+# set -x FZF_ALT_C_OPTS   "--preview 'tree -C {} | head -200'"
+# # ファイル検索で選択した項目の中身をプレビューに表示する
+# set -x FZF_CTRL_T_OPTS  "--preview 'head -100 {}'"
+#
+# # エラーコードは番号で表示する
+# set -g theme_show_exit_status yes
+# # Gitのahead情報を細かく表示する
+# set -g theme_display_git_ahead_verbose yes
--- dot_fish/fish/keybinds.fish
+++ dot_fish/fish/keybinds.fish
@@ -5,9 +5,8 @@ function fish_user_key_bindings
 	bind \c] peco_select_ghq_repository
   	bind \cG '__ghq_crtl_g'
 	# fzf
-	bind \ct '__fzf_find_file'
-  	bind \cr '__fzf_reverse_isearch'
-  	bind \cx '__fzf_find_and_execute'
-  	bind \ce '__fzf_cd'
-  	bind \cE '__fzf_cd_with_hidden'
+	/* bind \ct '__fzf_find_file'          */
+ /*  	bind \cr '__fzf_reverse_isearch'  */
+ /*  	bind \cx '__fzf_find_and_execute' */
+ /*  	bind \ce '__fzf_cd'               */
 end
