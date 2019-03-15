fef2985467e85edf8d3b8364b85da741071ff636
daisuke.oku <w.40141@gmail.com>
2019-03-15 15:48:21 +0900

    fish

--- .config/fish/conf.d/ghq_key_bindings.fish
+++
@@ -1,4 +0,0 @@
-bind \cg '__ghq_crtl_g'
-if bind -M insert >/dev/null 2>/dev/null
-    bind -M insert \cg '__ghq_crtl_g'
-end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,1 @@
-set -x XDG_CONFIG_HOME "~/.config"
-
-function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
-end
-
-set -U FZF_LEGACY_KEYBINDINGS 0
+set -x XDG_CONFIG_HOME $HOME/.config
