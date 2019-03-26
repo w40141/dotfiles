dccbc499b1ca70b5af275439f094554a95f6d63b
daisuke.oku <w.40141@gmail.com>
2019-03-27 08:49:26 +0900

    edit init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@ for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
+    [[ "$f" == ".gitmodules" ]] && continue
     echo "$f"
 	ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
