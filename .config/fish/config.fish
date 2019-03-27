e6a63ba4f2433c86bc18af0ca0b2f566d31f9a57
daisuke.oku <w.40141@gmail.com>
2019-03-27 17:23:33 +0900

    edit fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,6 +36,7 @@ set -g theme_show_exit_status no
 set -g fish_prompt_pwd_dir_length 1
 # set -g theme_project_dir_length 0
 set -g theme_newline_cursor yes
+set -g theme_newline_prompt '>>> '
 
 # set
 set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
