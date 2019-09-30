9a9b48119576acfd824c1878a648124eeb82918b
daisuke.oku <w.40141@gmail.com>
2019-09-30 15:15:35 +0900

    test config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
-set -x XDG_CONFIG_HOME $HOME/.config
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+# set -x XDG_CONFIG_HOME $HOME/.config
 
 alias la='ls -a'
 alias ll='ls -l'
