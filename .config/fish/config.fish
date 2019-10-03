a57ff3525234821ca81f5b56af2122d0a5e54c9c
daisuke.oku <w.40141@gmail.com>
2019-10-03 09:53:40 +0900

    add fish-nvm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
 set -gx XDG_CONFIG_HOME $HOME/.config
 
 alias la='ls -a'
