50f5c48f889ab1ba51cee41f0b8de47142a56025
daisuke.oku <w.40141@gmail.com>
2019-10-25 15:23:42 +0900

    edit config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -62,22 +62,21 @@ set -g theme_newline_prompt (set_color green)\uF558'  '
 set -gx SDKROOT (xcrun --sdk macosx --show-sdk-path)
 
 # PATH
-set -gx fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/apr/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/curl-openssl/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/libpq/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/nss/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/qt/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/sphinx-doc/bin" $fish_user_paths
-set -gx fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
-set -gx fish_user_paths "$HOME/development/flutter/bin" $fish_user_paths
-
-set -x fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/apr/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/curl-openssl/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/libpq/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/nss/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/qt/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/sphinx-doc/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+set -g fish_user_paths "$HOME/development/flutter/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (nodenv init -|psub)
