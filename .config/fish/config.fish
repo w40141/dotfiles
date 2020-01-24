6702d94dcf6f5b850256b935b95c1efd7eef1304
daisuke.oku <w.40141@gmail.com>
2020-01-24 14:34:34 +0900

    edit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -84,12 +84,3 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (rbenv init -|psub)
-
-function brew
-    set -xl PATH $PATH # Protect global PATH by local PATH
-    if type -q pyenv; and contains (pyenv root)/shims $PATH
-        set -e PATH[(contains -i (pyenv root)/shims $PATH)]
-    end
-
-    command brew $argv
-end
