268926b09dd96baa59028b01930fd881d1ac65e1
w40141 <w.40141@gmail.com>
2020-10-02 13:23:25 +0900

    edit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -63,32 +63,7 @@ status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (rbenv init -|psub)
 status --is-interactive; and source (nodenv init -|psub)
 
-set -g fish_user_paths '/usr/local/sbin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/openssl@1.1/bin' $fish_user_paths
-# set -g fish_user_paths '/usr/local/opt/ruby/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/apr/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/curl/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/curl-openssl/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/icu4c/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/icu4c/sbin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/libpq/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/llvm/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/nss/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/openssl/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/qt/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/sqlite/bin' $fish_user_paths
-set -g fish_user_paths '$HOME/development/flutter/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/libxml2/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/krb5/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/krb5/sbin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/sphinx-doc/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/openldap/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/openldap/sbin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/openjdk/bin' $fish_user_paths
-set -g fish_user_paths '/usr/local/opt/gnu-getopt/bin' $fish_user_paths
-set -g fish_user_paths '$HOME/.poetry/bin' $fish_user_paths
+register-python-argcomplete --shell fish pipx | source
 
-set -g PATH $PATH /usr/local/opt/python@3.8/bin
-# Created by `userpath` on 2020-07-21 01:25:03
-set -g PATH $PATH $HOME/.local/bin
-register-python-argcomplete --shell fish pipx | .
+# Created by `userpath` on 2020-10-02 03:59:27
+set PATH $PATH /Users/daisuke.oku/.local/bin
