949dfbcb829fe37bb384dc7e78b54787bf9ccce4
w40141 <w.40141@gmail.com>
2020-10-08 16:38:56 +0900

    edit

--- test_config/fish/completions/clitw.fish
+++
@@ -1,5 +0,0 @@
-function __fish_is_subcommand
-    test 1 -eq (count (string match -v -- '-*' (commandline -poc)))
-end
-
-complete -f -c clitw -n '__fish_is_subcommand' -a tw -d "Tweet"
--- test_config/fisher/github.com/w40141/fish-my-pwm/README.md
+++
@@ -1,6 +0,0 @@
-# Fish completions for My-pwm
-[My-pwm](https://github.com/w40141/my-pwm) is Passward generator
-
-### How to Use
-
-    fisher add w40141/fish-my-pwm
