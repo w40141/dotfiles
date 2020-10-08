980a8fde19c843c3f2ac3086ab6ec06f00ec691a
w40141 <w.40141@gmail.com>
2020-10-08 14:39:01 +0900

    edit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 set -gx XDG_CONFIG_HOME $HOME/.config
+
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx NVIM_PYTHON_LOG_FILE /tmp/nvim.log
@@ -26,7 +27,7 @@ function brew
 end
 
 # for peco
-set -x fish_plugins theme peco
+set -g fish_plugins theme peco
 
 function fish_user_key_bindings
     bind \cr peco_select_history # Bind for prco history to Ctrl+r
@@ -56,19 +57,39 @@ set -g fish_prompt_pwd_dir_length 0
 set -g theme_newline_cursor yes
 set -g theme_newline_prompt (set_color green)\uf0a9'  '
 
-# PATH
-set -g PYENV_ROOT $HOME/.pyenv
+status --is-interactive; and source (anyenv init -|psub)
 
-# export RUBY_CONFIGURE_OPTS="--with-openssl-dir=(brew --prefix openssl@1.1)"
-set -x RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@1.1)
+# set -gx RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@1.1)
 
-set -gx SDKROOT (xcrun --sdk macosx --show-sdk-path)
+# set -gx SDKROOT (xcrun --sdk macosx --show-sdk-path)
 
 # status --is-interactive; and source (pyenv init -|psub)
 # status --is-interactive; and source (rbenv init -|psub)
 # status --is-interactive; and source (nodenv init -|psub)
-status --is-interactive; and source (anyenv init -|psub)
+
+# set -U fish_user_paths /usr/local/sbin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/openssl@1.1/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/apr/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/curl/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/curl-openssl/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/icu4c/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/icu4c/sbin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/libpq/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/llvm/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/nss/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/openssl/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/qt/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/sqlite/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/libxml2/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/krb5/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/krb5/sbin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/sphinx-doc/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/openldap/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/openldap/sbin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/openjdk/bin $fish_user_paths
+# set -U fish_user_paths /usr/local/opt/gnu-getopt/bin $fish_user_paths
+
 # Created by `userpath` on 2020-10-02 03:59:27
-set PATH $PATH $HOME/.local/bin
+set -x PATH $PATH $HOME/.local/bin
 
 register-python-argcomplete --shell fish pipx | source
