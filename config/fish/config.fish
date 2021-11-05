c7e08d7fd04666a68e2f002a10bf2b0d9d45d9f0
w40141 <w.40141@gmail.com>
2021-11-05 15:01:10 +0900

    edit

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -223,14 +223,12 @@ fish_add_path /usr/local/opt/openldap/bin
 fish_add_path /usr/local/opt/openldap/sbin
 fish_add_path /usr/local/opt/gnu-getopt/bin
 fish_add_path /usr/local/opt/libxslt/bin
-fish_add_path /usr/local/opt/openssl@1.1/bin
-fish_add_path /usr/local/opt/openssl/bin
-fish_add_path /usr/local/opt/openssl@3/bin
+# fish_add_path /usr/local/opt/openssl@1.1/bin
+# fish_add_path /usr/local/opt/openssl/bin
+# fish_add_path /usr/local/opt/openssl@3/bin
 fish_add_path $HOME/.asdf/installs/poetry/1.1.7/bin
 
 # Created by `pipx` on 2021-09-04 03:41:50
 set PATH $PATH /Users/daisuke.oku/.local/bin
-
-. ~/.asdf/plugins/java/set-java-home.fish
 
 source /usr/local/opt/asdf/libexec/asdf.fish
