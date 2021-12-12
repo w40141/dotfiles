61fab6b825a3c1fff71fcbbcf039f89250bb3b65
w40141 <w.40141@gmail.com>
2021-12-12 14:04:20 +0900

    edit

--- config/nvim/plugin/kyazdani42.nvim-tree.lua
+++ config/nvim/plugin/kyazdani42.nvim-tree.lua
@@ -48,7 +48,7 @@ require'nvim-tree'.setup {
         height = 30,
         hide_root_folder = false,
         side = 'left',
-        auto_resize = false,
+        auto_resize = true,
         mappings = {
             custom_only = true,
             list = {
@@ -95,3 +95,90 @@ require'nvim-tree'.setup {
         require_confirm = true
     }
 }
+local g = vim.g
+-- 0 by default, closes the tree when you open a file
+g.nvim_tree_quit_on_open = 0
+-- 0 by default, this option shows indent markers when folders are open
+g.nvim_tree_indent_markers = 1
+-- 0 by default, will enable file highlight for git attributes (can be used without the icons).
+g.nvim_tree_git_hl = 0
+-- 0 by default, will enable folder and file icon highlight for opened files/directories.
+g.nvim_tree_highlight_opened_files = 1
+-- This is the default. See :help filename-modifiers for more options
+g.nvim_tree_root_folder_modifier = ':~'
+-- 0 by default, append a trailing slash to folder names
+g.nvim_tree_add_trailing = 1
+-- 0 by default, compact folders that only contain a single folder into one node in the file tree
+g.nvim_tree_group_empty = 1
+-- 0 by default, will disable the window picker.
+g.nvim_tree_disable_window_picker = 1
+-- one space by default, used for rendering the space between the icon and the filename.
+-- Use with caution, it could break rendering if you set an empty string depending on your font.
+g.nvim_tree_icon_padding = ' '
+-- defaults to ' ➛ '. used as a separator between symlinks' source and target.
+g.nvim_tree_symlink_arrow = ' >> '
+-- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
+g.nvim_tree_respect_buf_cwd = 1
+-- 1 by default, When creating files, sets the path of a file when cursor is
+-- on a closed folder to the parent folder when 0, and inside the folder when 1.
+g.nvim_tree_create_in_closed_folder = 0
+-- 1000 by default, control how often the tree can be refreshed,
+-- 1000 means the tree can be refresh once per 1000ms.
+g.nvim_tree_refresh_wait = 500
+-- Dictionary of buffer option names mapped to a list of option values that
+-- indicates to the window picker that the buffer's window should not be
+-- selectable.
+g.nvim_tree_window_picker_exclude = {
+    filetype = {
+        'notify',
+        'packer',
+        'qf',
+    },
+    buftype = {
+        'terminal'
+    }
+}
+-- If 0, do not show the icons for one of 'git' 'folder' and 'files'
+-- 1 by default, notice that if 'files' is 1, it will only display
+-- if nvim-web-devicons is installed and on your runtimepath.
+-- if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
+-- but this will not work when you set indent_markers (because of UI conflict)
+g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows = 1,
+}
+
+-- default will show icon by default if no icon is provided
+-- default shows no icon by default
+g.nvim_tree_icons = {
+    default = '',
+    symlink = '',
+    git = {
+        unstaged = "✹",
+        staged ="✚",
+        unmerged= "═",
+        renamed= "➜",
+        untracked= "✭",
+        deleted = "",
+        ignored =  "◌",
+    },
+    folder = {
+        arrow_open ="",
+        arrow_closed= "",
+        default ="",
+        open ="",
+        empty= "",
+        empty_open ="",
+        symlink = "",
+        symlink_open= "",
+    },
+}
+
+local api = vim.api
+api.nvim_set_keymap('n', '<c-e>', ':<c-u>NvimTreeToggle<cr>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<leader>r', ':<c-u>NvimTreeRefresh<cr>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<leader>', ':<c-u>NvimTreeFindFile<cr>', { noremap = true, silent = true })
+
+vim.cmd([[highlight NvimTreeFolderIcon guibg=blue]])
