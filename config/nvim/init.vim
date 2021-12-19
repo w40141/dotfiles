cf33ac6596ee9202e7c9c868856e06020ac83716
w40141 <w.40141@gmail.com>
2021-12-19 10:12:04 +0900

    edit

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -2,119 +2,119 @@ lua << EOF
 require('init')
 EOF
 
-let g:config_dir = empty($XDG_CONFIG_HOME) ? expand($HOME) . "/.config" : expand($XDG_CONFIG_HOME)
-let g:python3_host_prog = g:config_dir . "/nvim/neovim3/.venv/bin/python"
-
-" :e などでファイルを開く際にフォルダが存在しない場合は自動作成
-function! s:mkdir(dir, force)
-    if !isdirectory(a:dir) && (a:force ||
-        \ input(printf('"%s" does not exist. Create? [y/N]', a:dir)) =~? '^y\%[es]$')
-        call mkdir(iconv(a:dir, &encoding, &termencoding), 'p')
-    endif
-endfunction
-
-autocmd MyAutoCmd BufWritePre * call s:mkdir(expand('<afile>:p:h'), v:cmdbang)
-
-let s:plugvim  = expand($XDG_DATA_HOME) . "/nvim/site/autoload/plug.vim"
-
-if empty(glob(s:plugvim))
-    execute "!sh -c 'curl -fLo " .
-        \ s:plugvim .
-        \ " --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'"
-    autocmd MyAutoCmd VimEnter * PlugInstall --sync | source $MYVIMRC
-endif
-
-" https://github.com/rockerBOO/awesome-neovim
-" https://zenn.dev/hituzi_no_sippo/articles/871c06cdbc45b53181e3
-" https://zenn.dev/kawarimidoll/articles/8172a4c29a6653
-" https://zenn.dev/monaqa/articles/2020-12-22-vim-abbrev
-" https://qiita.com/pink_bangbi/items/8d9041bb6bc3473dbd20
-" https://zenn.dev/tamago324/articles/2021-12-16-lir-nvim-introduction
-" TODO package manager
-" https://github.com/wbthomason/packer.nvim
-" fzf
-" https://github.com/nvim-telescope/telescope.nvim
-" TODO misc
-" https://github.com/vim-skk/skkeleton
-" https://github.com/mopp/vim-operator-convert-case
-" https://github.com/folke/which-key.nvim
-" https://github.com/VonHeikemen/fine-cmdline.nvim
-" Plug 'MattesGroeger/vim-bookmarks'
-" Plug 'rcarriga/vim-ultest', { 'do': ':UpdateRemotePlugins' }
-" https://github.com/phaazon/hop.nvim
-call plug#begin('~/.vim/plugged')
-" Lua
-Plug 'MunifTanjim/nui.nvim'
-" Plug 'VonHeikemen/fine-cmdline.nvim'
-Plug 'kyazdani42/nvim-web-devicons'
-Plug 'akinsho/bufferline.nvim'
-Plug 'akinsho/toggleterm.nvim'
-Plug 'nvim-lua/plenary.nvim'
-Plug 'tamago324/lir.nvim'
-" VimScript
-" Disable
-" Plug 'goolord/alpha-nvim'
-" Plug 'andymass/vim-matchup'
-" Plug 'echasnovski/mini.nvim'
-Plug '4513ECHO/vim-readme-viewer', { 'on': 'PlugReadme' }
-Plug 'airblade/vim-rooter'
-Plug 'windwp/nvim-ts-autotag'
-Plug 'blackcauldron7/surround.nvim'
-Plug 'dhruvasagar/vim-table-mode', { 'for': ['markdown'] }
-Plug 'folke/todo-comments.nvim'
-Plug 'gelguy/wilder.nvim', { 'do': ':UpdateRemotePlugins' }
-Plug 'glidenote/memolist.vim'
-Plug 'famiu/bufdelete.nvim'
-Plug 'haishanh/night-owl.vim'
-Plug 'honza/vim-snippets'
-Plug 'itchyny/vim-cursorword'
-Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
-Plug 'junegunn/fzf.vim'
-Plug 'junegunn/goyo.vim'
-Plug 'junegunn/rainbow_parentheses.vim', {'on': 'RainbowParentheses' }
-Plug 'junegunn/vim-easy-align'
-Plug 'kamykn/spelunker.vim'
-Plug 'kana/vim-operator-replace'
-Plug 'kana/vim-operator-user'
-Plug 'kana/vim-textobj-user'
-Plug 'kevinhwang91/nvim-hlslens'
-" Plug 'kyazdani42/nvim-tree.lua'
-Plug 'lewis6991/gitsigns.nvim'
-Plug 'liuchengxu/vista.vim'
-Plug 'lukas-reineke/indent-blankline.nvim'
-Plug 'neoclide/coc.nvim', { 'branch': 'release' }
-Plug 'ntpeters/vim-better-whitespace'
-Plug 'numToStr/Comment.nvim'
-Plug 'nvim-lualine/lualine.nvim'
-Plug 'nvim-treesitter/nvim-treesitter'
-Plug 'nvim-treesitter/nvim-treesitter-textobjects'
-Plug 'osyo-manga/vim-precious'
-Plug 'osyo-manga/vim-textobj-blockwise'
-Plug 'osyo-manga/vim-textobj-multiblock'
-Plug 'previm/previm', { 'for': ['markdown'] }
-Plug 'reireias/vim-cheatsheet', { 'on': 'Cheat' }
-Plug 'rust-lang/rust.vim', { 'for': ['rust'] }
-Plug 'segeljakt/vim-silicon', { 'on': 'Silicon' }
-Plug 'Shougo/context_filetype.vim'
-Plug 'Shougo/vimproc.vim', { 'do': 'make' }
-Plug 'simeji/winresizer', { 'on': 'WinResizerStartResize' }
-Plug 'skanehira/translate.vim'
-Plug 't9md/vim-quickhl', { 'on': ['<plug>(quickhl-manual-this)', '<plug>(quickhl-manual-reset)'] }
-Plug 'terryma/vim-expand-region', { 'on': '<plug>(expand_region_' }
-Plug 'thinca/vim-qfreplace'
-Plug 'thinca/vim-quickrun'
-Plug 'thinca/vim-textobj-between'
-Plug 'tpope/vim-fugitive'
-Plug 'tpope/vim-repeat', { 'on': ['FocusLost', 'CursorHold'] }
-Plug 'tweekmonster/startuptime.vim', { 'on': 'StartupTime' }
-Plug 'tyru/eskk.vim'
-Plug 'tyru/open-browser.vim'
-Plug 'vim-jp/vimdoc-ja'
-Plug 'vim-test/vim-test'
-call plug#end()
-
-let s:plugs = get(s:, 'plugs', get(g:, 'plugs', {}))
-function! FindPlugin(name) abort
-    return has_key(s:plugs, a:name) ? isdirectory(s:plugs[a:name].dir) : 0
-endfunction
-command! -nargs=1 UsePlugin if !FindPlugin(<args>) | finish | endif
+" let g:config_dir = empty($XDG_CONFIG_HOME) ? expand($HOME) . "/.config" : expand($XDG_CONFIG_HOME)
+" let g:python3_host_prog = g:config_dir . "/nvim/neovim3/.venv/bin/python"
+" 
+" " :e などでファイルを開く際にフォルダが存在しない場合は自動作成
+" function! s:mkdir(dir, force)
+"     if !isdirectory(a:dir) && (a:force ||
+"         \ input(printf('"%s" does not exist. Create? [y/N]', a:dir)) =~? '^y\%[es]$')
+"         call mkdir(iconv(a:dir, &encoding, &termencoding), 'p')
+"     endif
+" endfunction
+" 
+" autocmd MyAutoCmd BufWritePre * call s:mkdir(expand('<afile>:p:h'), v:cmdbang)
+" 
+" let s:plugvim  = expand($XDG_DATA_HOME) . "/nvim/site/autoload/plug.vim"
+" 
+" if empty(glob(s:plugvim))
+"     execute "!sh -c 'curl -fLo " .
+"         \ s:plugvim .
+"         \ " --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'"
+"     autocmd MyAutoCmd VimEnter * PlugInstall --sync | source $MYVIMRC
+" endif
+" 
+" " https://github.com/rockerBOO/awesome-neovim
+" " https://zenn.dev/hituzi_no_sippo/articles/871c06cdbc45b53181e3
+" " https://zenn.dev/kawarimidoll/articles/8172a4c29a6653
+" " https://zenn.dev/monaqa/articles/2020-12-22-vim-abbrev
+" " https://qiita.com/pink_bangbi/items/8d9041bb6bc3473dbd20
+" " https://zenn.dev/tamago324/articles/2021-12-16-lir-nvim-introduction
+" " TODO package manager
+" " https://github.com/wbthomason/packer.nvim
+" " fzf
+" " https://github.com/nvim-telescope/telescope.nvim
+" " TODO misc
+" " https://github.com/vim-skk/skkeleton
+" " https://github.com/mopp/vim-operator-convert-case
+" " https://github.com/folke/which-key.nvim
+" " https://github.com/VonHeikemen/fine-cmdline.nvim
+" " Plug 'MattesGroeger/vim-bookmarks'
+" " Plug 'rcarriga/vim-ultest', { 'do': ':UpdateRemotePlugins' }
+" " https://github.com/phaazon/hop.nvim
+" call plug#begin('~/.vim/plugged')
+" " Lua
+" Plug 'MunifTanjim/nui.nvim'
+" " Plug 'VonHeikemen/fine-cmdline.nvim'
+" Plug 'kyazdani42/nvim-web-devicons'
+" Plug 'akinsho/bufferline.nvim'
+" Plug 'akinsho/toggleterm.nvim'
+" Plug 'nvim-lua/plenary.nvim'
+" Plug 'tamago324/lir.nvim'
+" " VimScript
+" " Disable
+" " Plug 'goolord/alpha-nvim'
+" " Plug 'andymass/vim-matchup'
+" " Plug 'echasnovski/mini.nvim'
+" Plug '4513ECHO/vim-readme-viewer', { 'on': 'PlugReadme' }
+" Plug 'airblade/vim-rooter'
+" Plug 'windwp/nvim-ts-autotag'
+" Plug 'blackcauldron7/surround.nvim'
+" Plug 'dhruvasagar/vim-table-mode', { 'for': ['markdown'] }
+" Plug 'folke/todo-comments.nvim'
+" Plug 'gelguy/wilder.nvim', { 'do': ':UpdateRemotePlugins' }
+" Plug 'glidenote/memolist.vim'
+" Plug 'famiu/bufdelete.nvim'
+" Plug 'haishanh/night-owl.vim'
+" Plug 'honza/vim-snippets'
+" Plug 'itchyny/vim-cursorword'
+" Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+" Plug 'junegunn/fzf.vim'
+" Plug 'junegunn/goyo.vim'
+" Plug 'junegunn/rainbow_parentheses.vim', {'on': 'RainbowParentheses' }
+" Plug 'junegunn/vim-easy-align'
+" Plug 'kamykn/spelunker.vim'
+" Plug 'kana/vim-operator-replace'
+" Plug 'kana/vim-operator-user'
+" Plug 'kana/vim-textobj-user'
+" Plug 'kevinhwang91/nvim-hlslens'
+" " Plug 'kyazdani42/nvim-tree.lua'
+" Plug 'lewis6991/gitsigns.nvim'
+" Plug 'liuchengxu/vista.vim'
+" Plug 'lukas-reineke/indent-blankline.nvim'
+" Plug 'neoclide/coc.nvim', { 'branch': 'release' }
+" Plug 'ntpeters/vim-better-whitespace'
+" Plug 'numToStr/Comment.nvim'
+" Plug 'nvim-lualine/lualine.nvim'
+" Plug 'nvim-treesitter/nvim-treesitter'
+" Plug 'nvim-treesitter/nvim-treesitter-textobjects'
+" Plug 'osyo-manga/vim-precious'
+" Plug 'osyo-manga/vim-textobj-blockwise'
+" Plug 'osyo-manga/vim-textobj-multiblock'
+" Plug 'previm/previm', { 'for': ['markdown'] }
+" Plug 'reireias/vim-cheatsheet', { 'on': 'Cheat' }
+" Plug 'rust-lang/rust.vim', { 'for': ['rust'] }
+" Plug 'segeljakt/vim-silicon', { 'on': 'Silicon' }
+" Plug 'Shougo/context_filetype.vim'
+" Plug 'Shougo/vimproc.vim', { 'do': 'make' }
+" Plug 'simeji/winresizer', { 'on': 'WinResizerStartResize' }
+" Plug 'skanehira/translate.vim'
+" Plug 't9md/vim-quickhl', { 'on': ['<plug>(quickhl-manual-this)', '<plug>(quickhl-manual-reset)'] }
+" Plug 'terryma/vim-expand-region', { 'on': '<plug>(expand_region_' }
+" Plug 'thinca/vim-qfreplace'
+" Plug 'thinca/vim-quickrun'
+" Plug 'thinca/vim-textobj-between'
+" Plug 'tpope/vim-fugitive'
+" Plug 'tpope/vim-repeat', { 'on': ['FocusLost', 'CursorHold'] }
+" Plug 'tweekmonster/startuptime.vim', { 'on': 'StartupTime' }
+" Plug 'tyru/eskk.vim'
+" Plug 'tyru/open-browser.vim'
+" Plug 'vim-jp/vimdoc-ja'
+" Plug 'vim-test/vim-test'
+" call plug#end()
+" 
+" let s:plugs = get(s:, 'plugs', get(g:, 'plugs', {}))
+" function! FindPlugin(name) abort
+"     return has_key(s:plugs, a:name) ? isdirectory(s:plugs[a:name].dir) : 0
+" endfunction
+" command! -nargs=1 UsePlugin if !FindPlugin(<args>) | finish | endif
--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -29,10 +29,19 @@ local api = vim.api
 g.mapleader = ' '
 g.maplocalleader = ' '
 
--- 移動系
+-- [nore]map
 -- 行頭と行末への移動
 api.nvim_set_keymap('', '0', '$', { noremap = true, silent = true })
 api.nvim_set_keymap('', '1', '0', { noremap = true, silent = true })
+-- ;と:を入れ替
+api.nvim_set_keymap('', ';', ':', { noremap = true, silent = false })
+api.nvim_set_keymap('', ':', ';', { noremap = true, silent = false })
+-- キー置換
+api.nvim_set_keymap('', ',h', '^', { noremap = true, silent = true })
+api.nvim_set_keymap('', ',l', '$', { noremap = true, silent = true })
+api.nvim_set_keymap('', ',m', '%', { noremap = true, silent = true })
+
+-- n[nore]map
 -- 表示上の移動を可能にする
 api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
 api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
@@ -41,26 +50,18 @@ api.nvim_set_keymap('n', 'gk', 'k', { noremap = true, silent = true })
 -- 水平方向の移動を簡単にする
 api.nvim_set_keymap('n', 'zl', 'zL', { noremap = true, silent = true })
 api.nvim_set_keymap('n', 'zh', 'zH', { noremap = true, silent = true })
--- 挿入モードでの移動
-api.nvim_set_keymap('i', '<c-f>', '<right>', { noremap = true, silent = true })
-api.nvim_set_keymap('i', '<c-b>', '<left>', { noremap = true, silent = true })
-api.nvim_set_keymap('i', '<c-a>', '<home>', { noremap = true, silent = true })
-api.nvim_set_keymap('i', '<c-e>', '<end>', { noremap = true, silent = true })
-api.nvim_set_keymap('i', 'jj', '<esc>:<c-u>w<cr>', { noremap = true, silent = true })
-
--- 編集系
--- ;と:を入れ替
-api.nvim_set_keymap('', ';', ':', { noremap = true, silent = false })
-api.nvim_set_keymap('', ':', ';', { noremap = true, silent = false })
 -- キー置換
-api.nvim_set_keymap('', ',h', '^', { noremap = true, silent = true })
-api.nvim_set_keymap('', ',l', '$', { noremap = true, silent = true })
-api.nvim_set_keymap('', ',m', '%', { noremap = true, silent = true })
-api.nvim_set_keymap('n', ',/', ';', { noremap = true, silent = true })
--- ノーマルモードでも改行可能
-api.nvim_set_keymap('n', '<cr>', 'i<cr><esc>', { noremap = true, silent = true })
+-- api.nvim_set_keymap('n', ',/', ';', { noremap = true, silent = true })
+-- カーソル下の単語を小文字にする
+api.nvim_set_keymap('n', ',u', [[mzg~iw`z<Cmd>delmarks z<CR>]], { noremap = true, silent = true })
+-- カーソル下の単語を大文字にする
+api.nvim_set_keymap('n', ',U', [[mzlbg~l`z<Cmd>delmarks z<CR>]], { noremap = true, silent = true })
 -- 保存
 api.nvim_set_keymap('n', ',w', ':w<cr>', { noremap = true, silent = true })
+-- 検索結果のハイライトをC-h連打でクリアする
+api.nvim_set_keymap('n', ',n', ':<c-u>nohlsearch<cr>', { noremap = true, silent = true })
+-- 辞書
+api.nvim_set_keymap('n', ',?', ':!open dict://<cword><cr>', { noremap = true, silent = true })
 -- 検索後にジャンプした際に検索単語を画面中央に持ってくる
 api.nvim_set_keymap('n', 'n', 'nzz', { noremap = true, silent = true })
 api.nvim_set_keymap('n', 'N', 'Nzz', { noremap = true, silent = true })
@@ -68,38 +69,14 @@ api.nvim_set_keymap('n', '*', '*zz', { noremap = true, silent = true })
 api.nvim_set_keymap('n', '#', '#zz', { noremap = true, silent = true })
 api.nvim_set_keymap('n', 'g*', 'g*zz', { noremap = true, silent = true })
 api.nvim_set_keymap('n', 'g#', 'g#zz', { noremap = true, silent = true })
--- 検索結果のハイライトをC-h連打でクリアする
-api.nvim_set_keymap('n', '<esc><esc>', ':<c-u>nohlsearch<cr>', { noremap = true, silent = true })
--- vを二回で行末まで選択
-api.nvim_set_keymap('v', 'v', '$h', { noremap = true, silent = true })
--- tabにて対応ペアにジャンプ
-api.nvim_set_keymap('n', '<tab>', '%', { noremap = true, silent = true })
-api.nvim_set_keymap('v', '<tab>', '%', { noremap = true, silent = true })
-
--- 補助系
--- ctrl-v で insert/command モードで貼り付け
-api.nvim_set_keymap('i', '<c-v>', '<eSC>"*pa', { noremap = true, silent = true })
-api.nvim_set_keymap('c', '<c-v>', '<C-r>+', { noremap = true, silent = true })
--- Yで行末までヤンク
-api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true, silent = true })
--- カーソル位置の単語をyankする
-api.nvim_set_keymap('n', 'vy', 'viwy', { noremap = true, silent = true })
 -- カーソル位置の単語をyankした文字に置き換える
 api.nvim_set_keymap('n', 'vp', 'viwpviwy', { noremap = true, silent = true })
--- カーソル下の単語を * で検索
-api.nvim_set_keymap('v', '*', [["vy/\V<C-r>=substitute(escape(@v, '\/'), "\n", '\\n', 'g')<CR><CR>]], { noremap = true, silent = true })
--- コマンドモード時のカーソル移動
-api.nvim_set_keymap('c', '<c-a>', '<home>', { noremap = true, silent = false })
-api.nvim_set_keymap('c', '<c-b>', '<left>', { noremap = true, silent = false })
-api.nvim_set_keymap('c', '<c-d>', '<delete>', { noremap = true, silent = false })
-api.nvim_set_keymap('c', '<c-e>', '<end>', { noremap = true, silent = false })
-api.nvim_set_keymap('c', '<c-f>', '<right>', { noremap = true, silent = false })
-api.nvim_set_keymap('c', '<c-n>', '<down>', { noremap = true, silent = false })
-api.nvim_set_keymap('c', '<c-p>', '<up>', { noremap = true, silent = false })
--- 辞書
-api.nvim_set_keymap('n', ',?', ':!open dict://<cword><cr>', { noremap = true, silent = true })
-
--- ウィンドウ
+-- カーソル位置の単語をyankする
+api.nvim_set_keymap('n', 'vy', 'viwy', { noremap = true, silent = true })
+-- ノーマルモードでも改行可能
+-- api.nvim_set_keymap('n', '<cr>', 'i<cr><esc>', { noremap = true, silent = true })
+-- tabにて対応ペアにジャンプ
+api.nvim_set_keymap('n', '<tab>', '%', { noremap = true, silent = true })
 -- ウィンドウを分割
 api.nvim_set_keymap('n', 'sp', ':<c-u>sp<cr>', { noremap = true, silent = true })
 api.nvim_set_keymap('n', 'sv', ':<c-u>vs<cr>', { noremap = true, silent = true })
@@ -110,25 +87,12 @@ api.nvim_set_keymap('n', 'sl', '<c-w>l<cr>', { noremap = true, silent = true })
 api.nvim_set_keymap('n', 'sh', '<c-w>h<cr>', { noremap = true, silent = true })
 -- ウィンドウを閉じる
 api.nvim_set_keymap('n', 'sc', '<c-w>c<cr>', { noremap = true, silent = true })
+-- 現在のウィンドウ以外のウィンドウを閉じる
 api.nvim_set_keymap('n', 'so', '<c-w>o<cr>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'sq', ':<c-u>q<cr>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'ss', ':<c-u>w<cr>', { noremap = true, silent = true })
-
--- タブ
 -- 新規タブ
 api.nvim_set_keymap('n', 'tn', ':<c-u>tabnew<cr>', { noremap = true, silent = true })
--- 前のタブに切り替え
-api.nvim_set_keymap('n', '[t', 'gT', { noremap = true, silent = true })
--- 次のタブに切り替え
-api.nvim_set_keymap('n', ']t', 'gt', { noremap = true, silent = true })
-
--- バッファ
 -- バッファを閉じる
 api.nvim_set_keymap('n', 'bq', ':<c-u>bd<cr>', { noremap = true, silent = true })
--- バッファリストの1つ前のバッファを開く
--- api.nvim_set_keymap('n', 'b[', ':bprevious<cr>', { noremap = true, silent = true })
--- バッファリストの次のバッファを開く
--- api.nvim_set_keymap('n', 'b]', ':bnext<cr>', { noremap = true, silent = true })
 -- 直前のバッファを開く
 api.nvim_set_keymap('n', 'bb', ':b#<cr>', { noremap = true, silent = true })
 -- バッファリストの先頭を開く
@@ -137,32 +101,67 @@ api.nvim_set_keymap('n', 'bf', ':bf<cr>', { noremap = true, silent = true })
 api.nvim_set_keymap('n', 'bl', ':bl<cr>', { noremap = true, silent = true })
 --変更中の次のバッファへ移動
 api.nvim_set_keymap('n', 'bm', ':bm<cr>', { noremap = true, silent = true })
--- カレントのバッファを閉じてバッファリストから削除
--- api.nvim_set_keymap('n', 'bd', ':bdelete<cr>', { noremap = true, silent = true })
+-- レジスタを汚さない削除
+-- api.nvim_set_keymap('n', 'x', '_x', { noremap = true, silent =true })
+-- api.nvim_set_keymap('n', 'X', '_X', { noremap = true, silent =true })
+-- 保存して閉じる、保存せずに閉じるを無効
+api.nvim_set_keymap('n', 'ZZ', '<nop>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', 'ZQ', '<nop>', { noremap = true, silent = true })
+-- バッファリストの1つ前のバッファを開く
+api.nvim_set_keymap('n', '[b', ':bprevious<cr>', { noremap = true, silent = true })
+-- バッファリストの次のバッファを開く
+api.nvim_set_keymap('n', ']b', ':bnext<cr>', { noremap = true, silent = true })
+-- 前のタブに切り替え
+api.nvim_set_keymap('n', '[t', 'gT', { noremap = true, silent = true })
+-- 次のタブに切り替え
+api.nvim_set_keymap('n', ']t', 'gt', { noremap = true, silent = true })
+-- 矢印キー無効
+api.nvim_set_keymap('n', '<up>', '<nop>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<down>', '<nop>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<left>', '<nop>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<right>', '<nop>', { noremap = true, silent = true })
 
+-- i[nore]map
+-- 挿入モードでの移動
+api.nvim_set_keymap('i', '<c-a>', '<home>', { noremap = true, silent = true })
+api.nvim_set_keymap('i', '<c-b>', '<left>', { noremap = true, silent = true })
+api.nvim_set_keymap('i', '<c-e>', '<end>', { noremap = true, silent = true })
+api.nvim_set_keymap('i', '<c-f>', '<right>', { noremap = true, silent = true })
+-- ctrl-v で insert モードで貼り付け
+api.nvim_set_keymap('i', '<c-v>', '<eSC>"*pa', { noremap = true, silent = true })
+-- 矢印キー無効
+api.nvim_set_keymap('i', '<up>', '<nop>', { noremap = true, silent = true })
+api.nvim_set_keymap('i', '<down>', '<nop>', { noremap = true, silent = true })
+api.nvim_set_keymap('i', '<left>', '<nop>', { noremap = true, silent = true })
+api.nvim_set_keymap('i', '<right>', '<nop>', { noremap = true, silent = true })
 
--- その他
--- バックスラッシュやクエスチョンを状況に合わせ自動的にエスケープ
-api.nvim_set_keymap('c', [[/]], [[getcmdtype() == '/' ? '\/' : '/']], { noremap = true, silent = true, expr = true })
-api.nvim_set_keymap('c', [[?]], [[getcmdtype() == '?' ? '\?' : '?']], { noremap = true, silent = true, expr = true })
+-- v[nore]map
+-- vを二回で行末まで選択
+api.nvim_set_keymap('v', 'v', '$h', { noremap = true, silent = true })
+-- レジスタを汚さない削除
+-- api.nvim_set_keymap('v', 'x', '_x', { noremap = true, silent =true })
+-- api.nvim_set_keymap('v', 'X', '_X', { noremap = true, silent =true })
+-- 選択下を記号で囲う
 api.nvim_set_keymap('v', '{', [["zdi{<c-r>z}<esc>]], { noremap = true, silent = true })
 api.nvim_set_keymap('v', '[', [["zdi[<c-r>z]<esc>]], { noremap = true, silent = true })
 api.nvim_set_keymap('v', '(', [["zdi(<c-r>z)<esc>]], { noremap = true, silent = true })
 api.nvim_set_keymap('v', [[']], [["zdi'<c-r>z'<esc>]], { noremap = true, silent = true })
 api.nvim_set_keymap('v', [["]], [["zdi'<c-r>z'<esc>]], { noremap = true, silent = true })
 api.nvim_set_keymap('v', '--', [["zdi"<c-r>z^V"<esc>]], { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'q', 'gq', { noremap = true, silent = true })
+-- tabにて対応ペアにジャンプ
+api.nvim_set_keymap('v', '<tab>', '%', { noremap = true, silent = true })
 
--- 保存して閉じる、保存せずに閉じるを無効
-api.nvim_set_keymap('n', 'ZZ', '<nop>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'ZQ', '<nop>', { noremap = true, silent = true })
-
--- 矢印キー無効
-api.nvim_set_keymap('n', '<up>', '<nop>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', '<down>', '<nop>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', '<left>', '<nop>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', '<right>', '<nop>', { noremap = true, silent = true })
-api.nvim_set_keymap('i', '<up>', '<nop>', { noremap = true, silent = true })
-api.nvim_set_keymap('i', '<down>', '<nop>', { noremap = true, silent = true })
-api.nvim_set_keymap('i', '<left>', '<nop>', { noremap = true, silent = true })
-api.nvim_set_keymap('i', '<right>', '<nop>', { noremap = true, silent = true })
+-- c[nore]map
+-- コマンドモード時のカーソル移動
+api.nvim_set_keymap('c', '<c-a>', '<home>', { noremap = true, silent = false })
+api.nvim_set_keymap('c', '<c-b>', '<left>', { noremap = true, silent = false })
+api.nvim_set_keymap('c', '<c-d>', '<delete>', { noremap = true, silent = false })
+api.nvim_set_keymap('c', '<c-e>', '<end>', { noremap = true, silent = false })
+api.nvim_set_keymap('c', '<c-f>', '<right>', { noremap = true, silent = false })
+api.nvim_set_keymap('c', '<c-n>', '<down>', { noremap = true, silent = false })
+api.nvim_set_keymap('c', '<c-p>', '<up>', { noremap = true, silent = false })
+-- ctrl-v で command モードで貼り付け
+api.nvim_set_keymap('c', '<c-v>', '<C-r>+', { noremap = true, silent = true })
+-- バックスラッシュやクエスチョンを状況に合わせ自動的にエスケープ
+api.nvim_set_keymap('c', [[/]], [[getcmdtype() == '/' ? '\/' : '/']], { noremap = true, silent = true, expr = true })
+api.nvim_set_keymap('c', [[?]], [[getcmdtype() == '?' ? '\?' : '?']], { noremap = true, silent = true, expr = true })
