c6eca34979688cf7778448bb5c2a8a860ae27795
w40141 <w.40141@gmail.com>
2022-01-03 21:55:42 +0900

    edit

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -226,7 +226,7 @@ fish_add_path /usr/local/opt/libxslt/bin
 fish_add_path /usr/local/opt/openssl@1.1/bin
 fish_add_path /usr/local/opt/openssl@3/bin
 fish_add_path /usr/local/opt/ncurses/bin
-fish_add_path $HOME/.asdf/installs/poetry/1.1.7/bin
+fish_add_path $HOME/.asdf/installs/poetry/1.1.12/bin
 
 # Created by `pipx` on 2021-09-04 03:41:50
 set PATH $PATH /Users/daisuke.oku/.local/bin
