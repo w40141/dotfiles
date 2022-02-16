e153544cc576e537b90b86471925f1e959aead8c
w40141 <w.40141@gmail.com>
2022-02-16 12:55:43 +0900

    edit

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -112,9 +112,6 @@ key('n', 'bf', ':bf<cr>', { noremap = true, silent = true })
 key('n', 'bl', ':bl<cr>', { noremap = true, silent = true })
 --変更中の次のバッファへ移動
 key('n', 'bm', ':bm<cr>', { noremap = true, silent = true })
--- レジスタを汚さない削除
--- key('n', 'x', '_x', { noremap = true, silent =true })
--- key('n', 'X', '_X', { noremap = true, silent =true })
 -- 保存して閉じる、保存せずに閉じるを無効
 key('n', 'ZZ', '<nop>', { noremap = true, silent = true })
 key('n', 'ZQ', '<nop>', { noremap = true, silent = true })
@@ -152,8 +149,10 @@ key('i', '<right>', '<nop>', { noremap = true, silent = true })
 -- vを二回で行末まで選択
 key('v', 'v', '$h', { noremap = true, silent = true })
 -- レジスタを汚さない削除
--- key('v', 'x', '_x', { noremap = true, silent =true })
--- key('v', 'X', '_X', { noremap = true, silent =true })
+key('n', 'x', '_x', { noremap = true, silent =true })
+key('n', 'X', '_X', { noremap = true, silent =true })
+key('v', 'x', '_x', { noremap = true, silent =true })
+key('v', 'X', '_X', { noremap = true, silent =true })
 -- 選択下を記号で囲う
 -- key('v', '{', [["zdi{<c-r>z}<esc>]], { noremap = true, silent = true })
 -- key('v', '[', [["zdi[<c-r>z]<esc>]], { noremap = true, silent = true })
--- config/nvim/plugin/echasnovski.mini.nvim.lua
+++ config/nvim/plugin/echasnovski.mini.nvim.lua
@@ -18,7 +18,7 @@ require('mini.surround').setup({
         delete = 'sd', -- Delete surrounding
         find = 'sf', -- Find surrounding (to the right)
         find_left = 'sF', -- Find surrounding (to the left)
-        highlight = 'sh', -- Highlight surrounding
+        highlight = 'sH', -- Highlight surrounding
         replace = 'sr', -- Replace surrounding
         update_n_lines = 'sn', -- Update `n_lines`
     },
