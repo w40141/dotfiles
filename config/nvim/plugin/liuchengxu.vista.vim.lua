3f43d6072122975db8275f6b156b273e1cbd8c7b
w40141 <w.40141@gmail.com>
2022-02-16 16:19:13 +0900

    edit

--- config/nvim/plugin/liuchengxu.vista.vim.lua
+++ config/nvim/plugin/liuchengxu.vista.vim.lua
@@ -56,11 +56,46 @@ g['vista#renderer#enable_icon'] = 1
 
 -- The default icons can't be suitable for all the filetypes,
 -- you can extend it as you wish.
--- g['vista#renderer#icons'] = [[{"function":'\uf794', "variable":'\uf71b'}]]
 cmd [[
   let g:vista#renderer#icons = {
-    \   "function": "\uf794",
-    \   "variable": "\uf71b",
+    \    'func': "\uf794",
+    \    'function': "\uf794",
+    \    'functions': "\uf794",
+    \    'var': "\uf71b",
+    \    'variable': "\uf71b",
+    \    'variables': "\uf71b",
+    \    'const': "\uf8ff",
+    \    'constant': "\uf8ff",
+    \    'constructor': "\uf976",
+    \    'method': "\uf6a6",
+    \    'package': "\ue612",
+    \    'packages': "\ue612",
+    \    'enum': "\uf702",
+    \    'enummember': "\uf282",
+    \    'enumerator': "\uf702",
+    \    'module': "\uf136",
+    \    'modules': "\uf136",
+    \    'type': "\uf7fd",
+    \    'typedef': "\uf7fd",
+    \    'types': "\uf7fd",
+    \    'field': "\uf30b",
+    \    'fields': "\uf30b",
+    \    'macro': "\uf8a3",
+    \    'macros': "\uf8a3",
+    \    'map': "\ufb44",
+    \    'class': "\uf0e8",
+    \    'augroup': "\ufb44",
+    \    'struct': "\uf318",
+    \    'union': "\ufacd",
+    \    'member': "\uf02b",
+    \    'target': "\uf893",
+    \    'property': "\ufab6",
+    \    'interface': "\uf7fe",
+    \    'namespace': "\uf475",
+    \    'subroutine': "\uf9af",
+    \    'implementation': "\uf776",
+    \    'typeParameter': "\uf278",
+    \    'default': "\uf29c"
     \ }
 ]]
 cmd([[autocmd MyAutoCmd VimEnter * call vista#RunForNearestMethodOrFunction()]])
