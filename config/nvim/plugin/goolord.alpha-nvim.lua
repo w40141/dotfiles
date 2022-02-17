c27c65abc55f42bab398f6e1d409dc83cdb4afa0
w40141 <w.40141@gmail.com>
2022-02-17 09:56:49 +0900

    edit

--- config/nvim/plugin/goolord.alpha-nvim.lua
+++ config/nvim/plugin/goolord.alpha-nvim.lua
@@ -6,35 +6,36 @@ local dashboard = require 'alpha.themes.dashboard'
 
 -- Footer
 local function footer()
-  local version = vim.version()
-  local print_version = "v" .. version.major .. '.' .. version.minor .. '.' .. version.patch
-  local datetime = os.date('%Y/%m/%d %H:%M:%S')
+    local version = vim.version()
+    local print_version = "v" .. version.major .. '.' .. version.minor .. '.' .. version.patch
+    local datetime = os.date('%Y/%m/%d %H:%M:%S')
 
-  return print_version .. ' ' .. datetime
+    return print_version .. ' ' .. datetime
 end
 
 -- Banner
 local banner = {
-  "                                                    ",
-  " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-  " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-  " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-  " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-  " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-  " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-  "                                                    ",
+    "                                                    ",
+    " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                    ",
 }
 
 dashboard.section.header.val = banner
 
 -- Menu
 dashboard.section.buttons.val = {
-  dashboard.button('e', ' New file', ':ene <BAR> startinsert<CR>'),
-  dashboard.button('f', ' Find file', ":<c-u>CocCommand fzf-preview.FromResources buffer project_mru project<cr>"),
-  dashboard.button('o', 'פּ Open explorer', ":lua require'lir.float'.toggle()<cr>"),
-  dashboard.button('s', ' Settings', ':e $MYVIMRC<CR>'),
-  dashboard.button('u', ' Update plugins', ':PlugUpdate<CR>'),
-  dashboard.button('q', ' Quit', ':qa<CR>'),
+    dashboard.button('e', ' New file', ':ene <BAR> startinsert<CR>'),
+    dashboard.button('f', ' Find file', ":<c-u>CocCommand fzf-preview.FromResources buffer project_mru project<cr>"),
+    dashboard.button('o', 'פּ Open explorer', ":lua require'lir.float'.toggle()<cr>"),
+    dashboard.button('s', ' Settings', ':e $MYVIMRC<CR>'),
+    dashboard.button('c', '﫵Check health', ':checkhealth<cr>'),
+    dashboard.button('u', ' Update plugins', ':PlugUpdate<CR>'),
+    dashboard.button('q', ' Quit', ':qa<CR>'),
 }
 
 dashboard.section.footer.val = footer()
