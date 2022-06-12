0bec1a702a0348b64e9a5f0f50234ed3b232e777
w40141 <w.40141@gmail.com>
2022-06-12 13:05:13 +0900

    edit

--- config/nvim/plugin/rebelot.kanagawa.nvim.lua
+++ config/nvim/plugin/rebelot.kanagawa.nvim.lua
@@ -6,15 +6,17 @@ if vim.fn.has("termguicolors") == true then
 end
 require('kanagawa').setup({
     undercurl = true,           -- enable undercurls
-    commentStyle = "italic",
-    functionStyle = "NONE",
-    keywordStyle = "italic",
-    statementStyle = "bold",
-    typeStyle = "NONE",
-    variablebuiltinStyle = "italic",
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true},
+    statementStyle = { bold = true },
+    typeStyle = {},
+    variablebuiltinStyle = { italic = true},
     specialReturn = true,       -- special highlight for the return keyword
-    specialException = true,    -- special highlight for exception handling keywords 
+    specialException = true,    -- special highlight for exception handling keywords
     transparent = false,        -- do not set background color
+    dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
+    globalStatus = false,       -- adjust window separators highlight for laststatus=3
     colors = {},
     overrides = {},
 })
