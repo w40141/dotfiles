86831899eaaac54bfb6cad0750ccbb240380b19e
w40141 <w.40141@gmail.com>
2022-06-27 10:43:09 +0900

    edit

--- config/nvim/lua/plugin.lua
+++ config/nvim/lua/plugin.lua
@@ -1,72 +1,29 @@
--- On ly required if you have packer in your `opt` pack
-
-local cmd = vim.cmd
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
-return require('packer').startup(function(use)
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
-
-cmd([[command! PackerInstall packadd packer.nvim | lua require'packers'.install()]])
-cmd([[command! PackerUpdate packadd packer.nvim | lua require'packers'.update()]])
-cmd([[command! PackerSync packadd packer.nvim | lua require'packers'.sync()]])
-cmd([[command! PackerClean packadd packer.nvim | lua require'packers'.clean()]])
-cmd([[command! PackerCompile packadd packer.nvim | lua require'packers'.compile()]])
--- vim.cmd('packadd packer.nvim')
--- vim.cmd('augroup packer_user_config')
--- vim.cmd('autocmd!')
--- vim.cmd('autocmd BufWritePost plugins.lua source <afile> | PackerCompile')
--- vim.cmd('augroup end')
-
--- local packer_exists = pcall(cmd, [[packadd packer.nvim]])
---
--- if not packer_exists then
---     -- TODO: Maybe handle windows better?
---     if fn.input("Download Packer? (y for yes)") ~= "y" then
---         return
---     end
---
---     local directory = string.format(
---     '%s/site/pack/packer/opt/',
---     fn.stdpath('data')
---     )
---
---     fn.mkdir(directory, 'p')
---
---     local out = fn.system(string.format(
---     'git clone %s %s',
---     'https://github.com/wbthomason/packer.nvim',
---     directory .. '/packer.nvim'
---     ))
---
---     print(out)
---     print("Downloading packer.nvim...")
---
---     return
+-- -- On ly required if you have packer in your `opt` pack
+-- local install_path = vim.fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+-- if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+--   packer_bootstrap = vim.fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 -- end
 --
--- return require('packer').startup(
--- function(use)
---     -- Packer can manage itself
---     -- Packer can manage itself as an optional plugin
---     use {'wbthomason/packer.nvim', opt = true}
+-- require("packer").startup(function(use)
+--   use "wbthomason/packer.nvim"
+--   use 'kyazdani42/nvim-web-devicons'
+--   use({ "rcarriga/nvim-notify", module = "notify" })
+--   -- use({
+--   --     "folke/todo-comments.nvim",
+--   --     event = "VimEnter",
+--   --     config = function()
+--   --         require("rc/pluginconfig/todo-comments")
+--   --     end,
+--   -- })
 --
---     if packer_bootstrap then
---         require('packer').sync()
+--   if packer_bootstrap then
+--     require("packer").sync()
+--   end
+-- end)
+-- vim.cmd([[autocmd BufWritePost init.lua source <afile> | PackerCompile]])
+--
+-- local on_attach = function(client, bufnr)
+--     local function buf_set_keymap(...)
+--         vim.api.nvim_buf_set_keymap(bufnr, ...)
 --     end
 -- end
--- )
