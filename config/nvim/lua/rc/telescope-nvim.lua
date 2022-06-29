03d82cca106b8a287cf99e3d21629df0b88e765b
w40141 <w.40141@gmail.com>
2022-06-29 23:36:51 +0900

    edit

--- config/nvim/lua/rc/telescope-nvim.lua
+++ config/nvim/lua/rc/telescope-nvim.lua
@@ -59,34 +59,11 @@ require("telescope").setup({
             "--column",
             "--smart-case",
         },
-        layout_strategy = "flex",
-        layout_config = {
-            width = 0.8,
-            horizontal = {
-                mirror = false,
-                prompt_position = "top",
-                preview_cutoff = 120,
-                preview_width = 0.5,
-            },
-            vertical = {
-                mirror = false,
-                prompt_position = "top",
-                preview_cutoff = 120,
-                preview_width = 0.5,
-            },
-        },
         -- file_sorter = require("telescope.sorters").get_fuzzy_file,
         -- generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
         path_display = { "truncate" },
         dynamic_preview_title = true,
         winblend = 0,
-        border = {},
-        borderchars = {
-            { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-            prompt = { "─", "│", " ", "│", "┌", "┬", "│", "│" },
-            results = { "─", "│", "─", "│", "├", "┤", "┴", "└" },
-            preview = { "─", "│", "─", " ", "─", "┐", "┘", "─" },
-        },
         color_devicons = true,
         use_less = true,
         scroll_strategy = "cycle",
@@ -94,11 +71,10 @@ require("telescope").setup({
 
         buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
         mappings = {
-            n = { ["<C-p>"] = action_layout.toggle_preview },
+            n = { ["<C-t>"] = action_layout.toggle_preview },
             i = {
-                ["<C-p>"] = action_layout.toggle_preview,
+                ["<C-t>"] = action_layout.toggle_preview,
                 ["<C-x>"] = false,
-                ["<C-s>"] = actions.select_horizontal,
                 ["<Tab>"] = actions.toggle_selection + actions.move_selection_next,
                 ["<C-q>"] = actions.send_selected_to_qflist,
                 ["<CR>"] = actions.select_default + actions.center,
@@ -161,140 +137,3 @@ key("n", "[ff]gs", "<Cmd>lua require('telescope.builtin').git_status()<CR>", { n
 key("n", "[ff]gc", "<Cmd>lua require('telescope.builtin').git_commits()<CR>", { noremap = true, silent = true })
 key("n", "[ff]gC", "<Cmd>lua require('telescope.builtin').git_bcommits()<CR>", { noremap = true, silent = true })
 key("n", "[ff]gb", "<Cmd>lua require('telescope.builtin').git_branches()<CR>", { noremap = true, silent = true })
-
--- local function remove_duplicate_paths(tbl, cwd)
---     local res = {}
---     local hash = {}
---     for _, v in ipairs(tbl) do
---         local v1 = Path:new(v):normalize(cwd)
---         if not hash[v1] then
---             res[#res + 1] = v1
---             hash[v1] = true
---         end
---     end
---     return res
--- end
---
--- local function join_uniq(tbl, tbl2)
---     local res = {}
---     local hash = {}
---     for _, v1 in ipairs(tbl) do
---         res[#res + 1] = v1
---         hash[v1] = true
---     end
---
---     for _, v in pairs(tbl2) do
---         if not hash[v] then
---             table.insert(res, v)
---         end
---     end
---     return res
--- end
---
--- local function filter_by_cwd_paths(tbl, cwd)
---     local res = {}
---     local hash = {}
---     for _, v in ipairs(tbl) do
---         if v:find(cwd, 1, true) then
---             local v1 = Path:new(v):normalize(cwd)
---             if not hash[v1] then
---                 res[#res + 1] = v1
---                 hash[v1] = true
---             end
---         end
---     end
---     return res
--- end
---
--- local function requiref(module)
---     require(module)
--- end
---
--- telescope_builtin.my_mru = function(opts)
---     local get_mru = function(opts2)
---         local res = pcall(requiref, "telescope._extensions.frecency")
---         if not res then
---             return vim.tbl_filter(function(val)
---                 return 0 ~= vim.fn.filereadable(val)
---             end, vim.v.oldfiles)
---         else
---             local db_client = require("telescope._extensions.frecency.db_client")
---             db_client.init()
---             -- too slow
---             -- local tbl = db_client.get_file_scores(opts, vim.fn.getcwd())
---             local tbl = db_client.get_file_scores(opts2)
---             local get_filename_table = function(tbl2)
---                 local res2 = {}
---                 for _, v in pairs(tbl2) do
---                     res2[#res2 + 1] = v["filename"]
---                 end
---                 return res2
---             end
---             return get_filename_table(tbl)
---         end
---     end
---     local results_mru = get_mru(opts)
---     local results_mru_cur = filter_by_cwd_paths(results_mru, vim.loop.cwd())
---
---     local show_untracked = utils.get_default(opts.show_untracked, true)
---     local recurse_submodules = utils.get_default(opts.recurse_submodules, false)
---     if show_untracked and recurse_submodules then
---         error("Git does not suppurt both --others and --recurse-submodules")
---     end
---     local cmd = {
---         "git",
---         "ls-files",
---         "--exclude-standard",
---         "--cached",
---         show_untracked and "--others" or nil,
---         recurse_submodules and "--recurse-submodules" or nil,
---     }
---     local results_git = utils.get_os_command_output(cmd)
---
---     local results = join_uniq(results_mru_cur, results_git)
---
---     pickers.new(opts, {
---         prompt_title = "MRU",
---         finder = finders.new_table({
---             results = results,
---             entry_maker = opts.entry_maker or make_entry.gen_from_file(opts),
---         }),
---         -- default_text = vim.fn.getcwd(),
---         sorter = conf.file_sorter(opts),
---         previewer = conf.file_previewer(opts),
---     }):find()
--- end
---
--- telescope_builtin.grep_prompt = function(opts)
---     opts.search = vim.fn.input("Grep String > ")
---     telescope_builtin.my_grep(opts)
--- end
---
--- telescope_builtin.my_grep = function(opts)
---     require("telescope.builtin").grep_string({
---         opts = opts,
---         prompt_title = "grep_string: " .. opts.search,
---         search = opts.search,
---         use_regex = true,
---     })
--- end
---
--- telescope_builtin.my_grep_in_dir = function(opts)
---     opts.search = vim.fn.input("Grep String > ")
---     opts.search_dirs = {}
---     opts.search_dirs[1] = vim.fn.input("Target Directory > ")
---     require("telescope.builtin").grep_string({
---         opts = opts,
---         prompt_title = "grep_string(dir): " .. opts.search,
---         search = opts.search,
---         search_dirs = opts.search_dirs,
---     })
--- end
---
--- telescope_builtin.memo = function(opts)
---     require("telescope.builtin").find_files({
---         opts = opts,
---         prompt_title = "MemoList",
---         find_command = { "find", vim.g.memolist_path, "-type", "f", "-exec", "ls", "-1ta", "{}", "+" },
---     })
--- end
