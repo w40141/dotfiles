3c2ed0079463cb615309eab4536119c526cb3d6a
w40141 <w.40141@gmail.com>
2022-07-20 18:55:33 +0900

    edit

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,17 +5,16 @@ g.did_install_syntax_menu   = 1
 g.did_load_ftplugin         = 1
 
 g.loaded_2html_plugin       = 1
-g.loaded_gzip               = 1
-g.loaded_man                = 1
+g.loaded_gzip               = 0
+g.loaded_man                = 0
 g.loaded_matchit            = 1
-g.loaded_matchparen         = 1
 g.loaded_netrwPlugin        = 1
 g.loaded_shada_plugin       = 1
 g.loaded_spellfile_plugin   = 1
-g.loaded_tarPlugin          = 1
-g.loaded_tutor_mode_plugin  = 1
-g.loaded_zipPlugin          = 1
-g.skip_loading_mswin        = 1
+g.loaded_tarPlugin          = 0
+g.loaded_tutor_mode_plugin  = 0
+g.loaded_zipPlugin          = 0
+g.skip_loading_mswin        = 0
 
 g.loaded_perl_provider      = 0
 
