7190bfcd27298067295c648a37c4395dfdf0c7e3
w40141 <w.40141@gmail.com>
2022-12-05 11:27:34 +0900

    edit

--- config/nvim/lua/rc/mason-lspconfig.lua
+++ config/nvim/lua/rc/mason-lspconfig.lua
@@ -8,32 +8,48 @@ local buf = lsp.buf
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities(lsp.protocol.make_client_capabilities())
 local on_attach = function(_, bufnr)
-  -- local cap = client.server_capabilities.document_highlight
-  -- if cap then
-    hl(0, "LspReferenceText", { underline = true, bold = true, fg = "#A00000", bg = "#104040" })
-    hl(0, "LspReferenceRead", { underline = true, bold = true, fg = "#A00000", bg = "#104040" })
-    hl(0, "LspReferenceWrite", { underline = true, bold = true, fg = "#A00000", bg = "#104040" })
-    local ldh = augroup("LspDocumentHighlight", {})
-    autocmd({ "CursorHold", "CursorHoldI" }, {
-      buffer = bufnr,
-      callback = buf.document_highlight,
-      group = ldh,
-    })
-    autocmd({ "CursorMoved", "CursorMovedI" }, {
-      buffer = bufnr,
-      callback = buf.clear_references,
-      group = ldh,
-    })
-  -- end
+	-- local cap = client.server_capabilities.document_highlight
+	-- if cap then
+	hl(0, "LspReferenceText", { underline = true, bold = true, fg = "#A00000", bg = "#104040" })
+	hl(0, "LspReferenceRead", { underline = true, bold = true, fg = "#A00000", bg = "#104040" })
+	hl(0, "LspReferenceWrite", { underline = true, bold = true, fg = "#A00000", bg = "#104040" })
+	local ldh = augroup("LspDocumentHighlight", {})
+	autocmd({ "CursorHold", "CursorHoldI" }, {
+		buffer = bufnr,
+		callback = buf.document_highlight,
+		group = ldh,
+	})
+	autocmd({ "CursorMoved", "CursorMovedI" }, {
+		buffer = bufnr,
+		callback = buf.clear_references,
+		group = ldh,
+	})
+	-- end
 end
 
 require("mason-lspconfig").setup_handlers({
-  function(server)
-    local opts = { capabilities = capabilities, on_attach = on_attach }
-    require("lspconfig")[server].setup(opts)
-  end,
+	function(server)
+		local opts = { capabilities = capabilities, on_attach = on_attach }
+		require("lspconfig")[server].setup(opts)
+	end,
 })
 
 require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua", "rust_analyzer" },
+	ensure_installed = {
+		"sumneko_lua",
+		"rust_analyzer",
+		"bashls",
+		"eslint",
+		"jdtls",
+		"tsserver",
+		"graphql",
+		"kotlin_language_server",
+		"pyright",
+		"sqlls",
+		"yamlls",
+		"spectral",
+		"marksman",
+		"html",
+		"dockerls",
+	},
 })
