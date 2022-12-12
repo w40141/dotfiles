de47c89b30929b98761a7cf8df415b5ede719eb7
w40141 <w.40141@gmail.com>
2022-12-12 14:50:12 +0900

    edit

--- config/nvim/lua/rc/gitsigns-nvim.lua
+++ config/nvim/lua/rc/gitsigns-nvim.lua
@@ -62,9 +62,9 @@ require("gitsigns").setup({
 		changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
 		untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
 	},
-	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+	signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
+	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
 	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = { interval = 1000, follow_files = true },
 	attach_to_untracked = true,
