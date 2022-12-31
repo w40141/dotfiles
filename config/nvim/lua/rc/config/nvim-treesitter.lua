7ffc43a6e67d5625346fc2ad9c4974e3b32b3288
w40141 <w.40141@gmail.com>
2022-12-31 17:01:00 +0900

    edit

--- config/nvim/lua/rc/config/nvim-treesitter.lua
+++ config/nvim/lua/rc/config/nvim-treesitter.lua
@@ -1,8 +1,8 @@
 return {
 	function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-			sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+			ensure_installed = "all",
+			sync_install = false,
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = false,
