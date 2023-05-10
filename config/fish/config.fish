961589fbaddf18ce1560458ac923b053e617e7d2
w40141 <w.40141@gmail.com>
2023-05-10 18:30:19 +0900

    edit

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -284,3 +284,7 @@ function brew
 
     command brew $argv
 end
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
