36fa7c67631ca8f1da47f3b4c12c18fb4f3c5af8
w40141 <w.40141@gmail.com>
2024-04-23 10:46:02 +0900

    edit

--- config/nvim/lua/core/lazy.lua
+++ config/nvim/lua/core/lazy.lua
@@ -4,6 +4,10 @@ local v = vim
 -- TODO: https://github.com/tris203/hawtkeys.nvim
 -- TODO: https://github.com/yutkat/my-neovim-pluginlist/blob/main/search_replace_grep_select.md#replace
 -- TODO: https://zenn.dev/charlie/articles/421bd209099ea5
+-- TODO: https://github.com/rgroli/other.nvim
+-- TODO: https://github.com/nvim-orgmode/orgmode
+-- TODO: https://github.com/nvim-neorg/neorg
+
 local lazypath = v.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not v.loop.fs_stat(lazypath) then
 	v.fn.system({
