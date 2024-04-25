0c86d1eeb3e4c84863b62bd16f0d69805d3548e1
w40141 <w.40141@gmail.com>
2024-04-25 11:20:40 +0900

    edit

--- config/nvim/lua/core/lazy.lua
+++ config/nvim/lua/core/lazy.lua
@@ -1,12 +1,17 @@
 local v = vim
 
 -- TODO: https://github.com/epwalsh/pomo.nvim
+-- A simple, customizable pomodoro timer for Neovim, written in Lua,
+-- with nvim-notify, lualine, and telescope integrations
+
 -- TODO: https://github.com/tris203/hawtkeys.nvim
--- TODO: https://github.com/yutkat/my-neovim-pluginlist/blob/main/search_replace_grep_select.md#replace
--- TODO: https://zenn.dev/charlie/articles/421bd209099ea5
+--  Suggest new easy to hit keymaps and find issues with your current key map configurations
+
 -- TODO: https://github.com/rgroli/other.nvim
+-- Open alternative files for the current buffer
+
 -- TODO: https://github.com/nvim-orgmode/orgmode
--- TODO: https://github.com/nvim-neorg/neorg
+-- Orgmode clone written in Lua for Neovim 0.9+.
 
 local lazypath = v.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not v.loop.fs_stat(lazypath) then
