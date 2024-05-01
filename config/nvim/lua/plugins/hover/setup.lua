9b3f1420a3827506427949d2c09683c8a4fe4086
w40141 <w.40141@gmail.com>
2024-05-01 10:23:57 +0900

    edit

--- config/nvim/lua/plugins/hover/setup.lua
+++ config/nvim/lua/plugins/hover/setup.lua
@@ -2,16 +2,19 @@ local M = {}
 
 function M.hover()
 	local v = vim
+	local api = v.api
 	local key = v.keymap.set
 
-	local function b(name)
-		return function()
-			return require("hover")[name]()
+	key("n", "K", function()
+		local hover_win = v.b.hover_preview
+		if hover_win and api.nvim_win_is_valid(hover_win) then
+			api.nvim_set_current_win(hover_win)
+		else
+			require("hover").hover()
 		end
-	end
+	end, { desc = "hover.nvim" })
 
-	key("n", "H", b("hover"), { desc = "hover.nvim" })
-	key("n", "gH", b("hover_select"), { desc = "hover.nvim (select)" })
+	key("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
 end
 
 return M
