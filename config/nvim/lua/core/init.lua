548d5ffddda6b02faa5156d00667c0f3c8c000ba
w40141 <w.40141@gmail.com>
2024-06-24 13:10:04 +0900

    edit

--- config/nvim/lua/core/init.lua
+++ config/nvim/lua/core/init.lua
@@ -1,6 +1,9 @@
 local v = vim
 local g = v.g
 
+g.did_install_default_menus = 1
+g.did_install_syntax_menu = 1
+
 g.loaded_2html_plugin = 1
 
 g.loaded_gzip = 1
@@ -19,6 +22,8 @@ g.loaded_netrwSettings = 1
 
 g.loaded_getscript = 1
 g.loaded_getscriptPlugin = 1
+
+g.loaded_osc52 = 1
 
 g.did_indent_on = 1
 g.did_install_default_menus = 1
@@ -43,7 +48,7 @@ g.loaded_ruby_provider = 0
 -- g.did_load_filetypes = 1
 -- g.loaded_spellfile_plugin = 1
 -- g.loaded_sql_completion = 1
--- g.loaded_syntax_completion = 1
+g.loaded_syntax_completion = 1
 
 require("core.option")
 require("core.keymap")
