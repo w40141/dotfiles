975d24ad2b9ca76b77bc81615e713aabf9a066a7
w40141 <w.40141@gmail.com>
2024-10-22 11:06:02 +0900

    edit

--- config/nvim/lua/core/plugin.lua
+++ config/nvim/lua/core/plugin.lua
@@ -1,20 +1,10 @@
 local v = vim
 
--- TODO: https://github.com/epwalsh/pomo.nvim
--- A simple, customizable pomodoro timer for Neovim, written in Lua,
--- with nvim-notify, lualine, and telescope integrations
-
--- TODO: https://github.com/tris203/hawtkeys.nvim
---  Suggest new easy to hit keymaps and find issues with your current key map configurations
-
 -- TODO: https://github.com/folke/which-key.nvim
 -- Create key bindings that stick. WhichKey is a lua plugin for Neovim 0.5 that displays a popup with possible keybindings of the command you started typing.
 
--- TODO: https://github.com/tani/dmacro.nvim
--- neovim version of dmacro.el, which makes a key macro on-the-fly.
-
--- TODO: https://github.com/zk-org/zk-nvim
--- Neovim extension for zk
+-- TODO: AI
+-- https://github.com/yetone/avante.nvim
 
 local lazypath = v.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (v.uv or v.loop).fs_stat(lazypath) then
