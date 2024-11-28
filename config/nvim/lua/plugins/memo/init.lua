3d9f0853f51d8dfc1849f95d2e5f16c179b5e885
w40141 <w.40141@gmail.com>
2024-11-28 14:59:28 +0900

    edit

--- config/nvim/lua/plugins/memo/init.lua
+++ config/nvim/lua/plugins/memo/init.lua
@@ -21,37 +21,37 @@ return {
 		config = conf.memolist,
 		init = setup.memolist,
 	},
-	{
-		"epwalsh/obsidian.nvim",
-		-- url = "https://github.com/epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
-		-- lazy = true,
-		ft = "markdown",
-		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-		event = {
-			-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-			-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-			-- "BufReadPre ~/ghq/github/w40141/vault/**.md",
-			"BufNewFile ~/ghq/github/w40141/vault/**.md",
-		},
-		cmd = {
-			"ObsidianCheck",
-			"ObsidianToday",
-			"ObsidianYesterday",
-			"ObsidianTomorrow",
-			"ObsidianNew",
-			"ObsidianOpen",
-			"ObsidianBacklinks",
-			"ObsidianSearch",
-			"ObsidianTemplate",
-			"ObsidianQuickSwitch",
-			"ObsidianLinkNew",
-			"ObsidianLink",
-			"ObsidianFollowLink",
-			"ObsidianWorkspace",
-			"ObsidianRename",
-			"ObsidianPasteImg",
-		},
-		config = conf.obsidian,
-	},
+	-- {
+	-- 	"epwalsh/obsidian.nvim",
+	-- 	-- url = "https://github.com/epwalsh/obsidian.nvim",
+	-- 	version = "*", -- recommended, use latest release instead of latest commit
+	-- 	-- lazy = true,
+	-- 	ft = "markdown",
+	-- 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- 	event = {
+	-- 		-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	-- 		-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+	-- 		-- "BufReadPre ~/ghq/github/w40141/vault/**.md",
+	-- 		"BufNewFile ~/ghq/github/w40141/vault/**.md",
+	-- 	},
+	-- 	cmd = {
+	-- 		"ObsidianCheck",
+	-- 		"ObsidianToday",
+	-- 		"ObsidianYesterday",
+	-- 		"ObsidianTomorrow",
+	-- 		"ObsidianNew",
+	-- 		"ObsidianOpen",
+	-- 		"ObsidianBacklinks",
+	-- 		"ObsidianSearch",
+	-- 		"ObsidianTemplate",
+	-- 		"ObsidianQuickSwitch",
+	-- 		"ObsidianLinkNew",
+	-- 		"ObsidianLink",
+	-- 		"ObsidianFollowLink",
+	-- 		"ObsidianWorkspace",
+	-- 		"ObsidianRename",
+	-- 		"ObsidianPasteImg",
+	-- 	},
+	-- 	config = conf.obsidian,
+	-- },
 }
