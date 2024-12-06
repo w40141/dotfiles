0be6e0657eb78db78a7727b909bf5ed2b4533952
w40141 <w.40141@gmail.com>
2024-12-06 18:10:23 +0900

    edit

--- config/nvim/lua/core/plugin.lua
+++ config/nvim/lua/core/plugin.lua
@@ -6,6 +6,9 @@ local v = vim
 -- TODO: AI
 -- https://github.com/yetone/avante.nvim
 
+-- TODO: todo.txt
+-- https://github.com/arnarg/todotxt.nvim
+
 local lazypath = v.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (v.uv or v.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
