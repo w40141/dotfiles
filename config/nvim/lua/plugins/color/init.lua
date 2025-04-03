52e120d9d07da641ca9fdcc079542345d7679a36
w40141 <w.40141@gmail.com>
2025-04-03 10:19:11 +0900

    edit

--- config/nvim/lua/plugins/color/init.lua
+++ config/nvim/lua/plugins/color/init.lua
@@ -8,6 +8,7 @@ return {
 		config = conf.kanagawa,
 		lazy = false,
 		priority = 1000,
+		event = "VeryLazy",
 	},
 	{
 		"folke/tokyonight.nvim",
@@ -15,17 +16,20 @@ return {
 		lazy = false,
 		priority = 1000,
 		opts = {},
+		event = "VeryLazy",
 	},
 	{
 		"scottmckendry/cyberdream.nvim",
 		-- url = "https://github.com/scottmckendry/cyberdream.nvim",
 		lazy = false,
 		priority = 1000,
+		event = "VeryLazy",
 	},
 	{
 		"olimorris/onedarkpro.nvim",
 		-- url = "https://github.com/olimorris/onedarkpro.nvim",
 		lazy = false,
 		priority = 1000,
+		event = "VeryLazy",
 	},
 }
