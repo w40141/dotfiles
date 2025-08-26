a769abe1ed2b72a6f7ffa4f51dd5e9531678f762
w40141 <w.40141@gmail.com>
2025-08-26 11:07:46 +0900

    edit

--- config/nvim/lua/plugins/tool/init.lua
+++ config/nvim/lua/plugins/tool/init.lua
@@ -35,29 +35,29 @@ return {
 		-- url = "https://github.com/folke/zen-mode.nvim",
 		cmd = { "ZenMode" },
 	},
-	{
-		-- Colorizer
-		"norcalli/nvim-colorizer.lua",
-		-- url = "https://github.com/norcalli/nvim-colorizer.lua",
-		cmd = { "ColorizerToggle" },
-		opts = {
-			{
-				"css",
-				"javascript",
-				"typescript",
-			},
-			{
-				RGB = true, -- #RGB hex codes
-				RRGGBB = true, -- #RRGGBB hex codes
-				names = true, -- "Name" codes like Blue
-				RRGGBBAA = true, -- #RRGGBBAA hex codes
-				rgb_fn = false, -- CSS rgb() and rgba() functions
-				hsl_fn = false, -- CSS hsl() and hsla() functions
-				css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-				css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-			},
-		},
-	},
+	-- {
+	-- 	-- Colorizer
+	-- 	"norcalli/nvim-colorizer.lua",
+	-- 	-- url = "https://github.com/norcalli/nvim-colorizer.lua",
+	-- 	cmd = { "ColorizerToggle" },
+	-- 	opts = {
+	-- 		{
+	-- 			"css",
+	-- 			"javascript",
+	-- 			"typescript",
+	-- 		},
+	-- 		{
+	-- 			RGB = true, -- #RGB hex codes
+	-- 			RRGGBB = true, -- #RRGGBB hex codes
+	-- 			names = true, -- "Name" codes like Blue
+	-- 			RRGGBBAA = true, -- #RRGGBBAA hex codes
+	-- 			rgb_fn = false, -- CSS rgb() and rgba() functions
+	-- 			hsl_fn = false, -- CSS hsl() and hsla() functions
+	-- 			css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	-- 			css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+	-- 		},
+	-- 	},
+	-- },
 	{
 		"vinnymeller/swagger-preview.nvim",
 		-- url = "https://github.com/vinnymeller/swagger-preview.nvim",
