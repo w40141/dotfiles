1d4b6ef43c00a598c9107ef9d6572b7bead84b57
w40141 <w.40141@gmail.com>
2026-03-13 12:03:55 +0900

    edit

--- config/nvim/lua/plugins/treesitter/keys.lua
+++ config/nvim/lua/plugins/treesitter/keys.lua
@@ -7,35 +7,40 @@ M.treesitter_textobjects = {
 		function()
 			require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
 		end,
-		{ mode = { "x", "o" }, desc = "[treesitter] 関数全体を選択 (@function.outer)" },
+		mode = { "x", "o" },
+		desc = "[treesitter] 関数全体を選択 (@function.outer)",
 	},
 	{
 		"im",
 		function()
 			require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
 		end,
-		{ mode = { "x", "o" }, desc = "[treesitter] 関数内部のみを選択 (@function.inner)" },
+		mode = { "x", "o" },
+		desc = "[treesitter] 関数内部のみを選択 (@function.inner)",
 	},
 	{
 		"ac",
 		function()
 			require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
 		end,
-		{ mode = { "x", "o" }, desc = "[treesitter] クラス全体を選択 (@class.outer)" },
+		mode = { "x", "o" },
+		desc = "[treesitter] クラス全体を選択 (@class.outer)",
 	},
 	{
 		"ic",
 		function()
 			require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
 		end,
-		{ mode = { "x", "o" }, desc = "[treesitter] クラス内部のみを選択 (@class.inner)" },
+		mode = { "x", "o" },
+		desc = "[treesitter] クラス内部のみを選択 (@class.inner)",
 	},
 	{
 		"as",
 		function()
 			require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
 		end,
-		{ mode = { "x", "o" }, desc = "[treesitter] ローカルスコープを選択 (@local.scope)" },
+		mode = { "x", "o" },
+		desc = "[treesitter] ローカルスコープを選択 (@local.scope)",
 	},
 
 	-- swap (parameter)
@@ -44,14 +49,16 @@ M.treesitter_textobjects = {
 		function()
 			require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
 		end,
-		{ mode = "n", desc = "[treesitter] 次の引数と入れ替え (@parameter.inner)" },
+		mode = "n",
+		desc = "[treesitter] 次の引数と入れ替え (@parameter.inner)",
 	},
 	{
 		"<leader>A",
 		function()
 			require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.inner")
 		end,
-		{ mode = "n", desc = "[treesitter] 前の引数と入れ替え (@parameter.inner)" },
+		mode = "n",
+		desc = "[treesitter] 前の引数と入れ替え (@parameter.inner)",
 	},
 
 	-- move next start
@@ -60,35 +67,40 @@ M.treesitter_textobjects = {
 		function()
 			require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 次の関数の開始位置へ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 次の関数の開始位置へ移動",
 	},
 	{
 		"]]",
 		function()
 			require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 次のクラスの開始位置へ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 次のクラスの開始位置へ移動",
 	},
 	{
 		"]o",
 		function()
 			require("nvim-treesitter-textobjects.move").goto_next_start({ "@loop.inner", "@loop.outer" }, "textobjects")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 次のループの開始位置へ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 次のループの開始位置へ移動",
 	},
 	{
 		"]s",
 		function()
 			require("nvim-treesitter-textobjects.move").goto_next_start("@local.scope", "locals")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 次のローカルスコープへ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 次のローカルスコープへ移動",
 	},
 	{
 		"]z",
 		function()
 			require("nvim-treesitter-textobjects.move").goto_next_start("@fold", "folds")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 次のfoldブロックへ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 次のfoldブロックへ移動",
 	},
 
 	-- move next end
@@ -97,14 +109,16 @@ M.treesitter_textobjects = {
 		function()
 			require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 次の関数の終了位置へ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 次の関数の終了位置へ移動",
 	},
 	{
 		"][",
 		function()
 			require("nvim-treesitter-textobjects.move").goto_next_end("@class.outer", "textobjects")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 次のクラスの終了位置へ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 次のクラスの終了位置へ移動",
 	},
 
 	-- move previous start
@@ -113,14 +127,16 @@ M.treesitter_textobjects = {
 		function()
 			require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 前の関数の開始位置へ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 前の関数の開始位置へ移動",
 	},
 	{
 		"[[",
 		function()
 			require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 前のクラスの開始位置へ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 前のクラスの開始位置へ移動",
 	},
 
 	-- move previous end
@@ -129,14 +145,16 @@ M.treesitter_textobjects = {
 		function()
 			require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 前の関数の終了位置へ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 前の関数の終了位置へ移動",
 	},
 	{
 		"[]",
 		function()
 			require("nvim-treesitter-textobjects.move").goto_previous_end("@class.outer", "textobjects")
 		end,
-		{ mode = { "n", "x", "o" }, desc = "[treesitter] 前のクラスの終了位置へ移動" },
+		mode = { "n", "x", "o" },
+		desc = "[treesitter] 前のクラスの終了位置へ移動",
 	},
 }
 
