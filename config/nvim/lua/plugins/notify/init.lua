63e0010acf5eef50d20d0bd8e51ac595c1ca2e99
w40141 <w.40141@gmail.com>
2026-04-13 13:59:34 +0900

    edit

--- config/nvim/lua/plugins/notify/init.lua
+++ config/nvim/lua/plugins/notify/init.lua
@@ -1,16 +1,16 @@
 local opts = require("plugins.notify.opts")
 
 return {
-	{
-		"folke/noice.nvim",
-		-- url = "https://github.com/folke/noice.nvim",
-		event = "VeryLazy",
-		opts = opts.noice,
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-	},
+	-- {
+	-- 	"folke/noice.nvim",
+	-- 	-- url = "https://github.com/folke/noice.nvim",
+	-- 	event = "VeryLazy",
+	-- 	opts = opts.noice,
+	-- 	dependencies = {
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		"rcarriga/nvim-notify",
+	-- 	},
+	-- },
 	{
 		"rcarriga/nvim-notify",
 		-- url = "https://github.com/rcarriga/nvim-notify",
